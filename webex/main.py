c160aea881dc49ef6da0c0b70dad264ea1c2b87e
Tomoya Watanabe <tomoya.watanabe@ctc-g.co.jp>
2024-08-06 02:20:16 +0000

    format and modify README

--- webex/main.py
+++ webex/main.py
@@ -7,33 +7,32 @@ import os
 app = FastAPI()
 
 headers = {
-    'authorization': f"Bearer {os.environ.get('webex_token')}",
-    'content-type': "application/json"
+    "authorization": f"Bearer {os.environ.get('webex_token')}",
+    "content-type": "application/json",
 }
 
 
-def get_message(id:Dict):
+def get_message(id: Dict):
     url = f"https://webexapis.com/v1/messages/{id}"
     response = requests.get(url, headers=headers)
     return response.json().get("text")
 
 
-def post_message(room_id,message:Dict):
+def post_message(room_id, message: Dict):
     url = f"https://webexapis.com/v1/messages/"
     payload = {
         "roomId": room_id,
         "text": message,
     }
-    response = requests.post(url,headers=headers,data=json.dumps(payload))
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
     print(response.text)
 
+
 @app.post("/message")
-async def message(message:Dict):
+async def message(message: Dict):
     # print(message)
     print(headers)
     message_id = message.get("data").get("id")
     room_id = message.get("data").get("roomId")
-    print(message_id,room_id)
-    post_message(room_id,"hoge")
-
-
+    print(message_id, room_id)
+    post_message(room_id, "hoge")
