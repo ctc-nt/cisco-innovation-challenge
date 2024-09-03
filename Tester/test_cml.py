deda31418788e05acab6901191bf7e2fa6f2d834
Tomoya Watanabe <tomoya.watanabe@ctc-g.co.jp>
2024-09-03 01:41:10 +0000

    fix: test-endpoint

--- Tester/test_cml.py
+++ Tester/test_cml.py
@@ -3,6 +3,6 @@ import os
 import pytest
 
 def test_get_request_root():
-    url = f"http://cml:8000/"
+    url = f"http://cml:8000/docs/"
     response = requests.get(url)
     assert response.status_code == 200
--- Tester/test_webex.py
+++ Tester/test_webex.py
@@ -3,7 +3,7 @@ import os
 import pytest
 
 def test_get_request_root():
-    url = f"http://webex:8000/"
+    url = f"http://webex:8000/docs/"
     response = requests.get(url)
     assert response.status_code == 200
 
